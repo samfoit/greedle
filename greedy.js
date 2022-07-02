f2c0f566d3e4919d41252c506f6aacd5c9635aed
Sam Foit <smfoit4311@gmail.com>
2022-07-02 00:48:35 -0500

    Core Gameplay Loop

--- greedy.js
+++ greedy.js
@@ -1,17 +1,49 @@
-let dice = 6;
+
+
+function game(){
+    let dice = 6;
+    let score = 0;
+
+    while (dice > 0){
+        let answer = prompt("Current Score: " + score +
+        "\nNumber of dice: " + dice + "\nRoll? yes or no: ");
+        answer = answer.toLowerCase();
+
+        if (answer == 'yes'){
+            let greedy = roll(dice);
+    
+            if (greedy[0] == 0){
+                console.log("you skunked!");
+                return;
+            }
+    
+            score += greedy[0];
+            dice -= greedy[1];
+    
+            if (dice == 0)
+                dice = 6;
+        }
+        else {
+            console.log("you take " + score + " points!");
+            dice = 0;
+        }
+    }
+}
 
 function roll(dice){
     let rolls = [];
     for (let i = 0; i < dice; i++){
         rolls[i] = Math.floor((Math.random() * 6) + 1);
-        console.log("Roll " + (i + 1) + ": " + rolls[i]);
+        console.log(rolls[i]);
     }
+    let game = score(rolls);
 
-    console.log(score(rolls));
+    return game;
 }
 
 function score(rolls){
     let newScore = 0;
+    let diceUsed = 0;
 
     for (let i = 1; i < 7; i++){
         let count = findCount(i, rolls);
@@ -19,14 +51,17 @@ function score(rolls){
 
         if (triple > 0 && i != 1 || i != 5){
             newScore += triple * (i * 100);
+            diceUsed += triple * 3;
         }
         else {
             if (i == 1) {
+                diceUsed += count;
                 newScore += triple * 1000;
                 count -= triple * 3;
                 newScore += count * 100;
             }
-            else {
+            else if (i == 5) {
+                diceUsed += count;
                 newScore += triple * 500;
                 count -= triple * 3;
                 newScore += count * 50;
@@ -34,7 +69,7 @@ function score(rolls){
         }
     }
 
-    return newScore;
+    return [newScore, diceUsed];
 }
 
 function findCount(num, rolls){
