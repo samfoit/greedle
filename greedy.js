0b3df8f4d631176360048908c1a06a41f6c5e36a
Sam Foit <smfoit4311@gmail.com>
2022-08-06 15:56:33 -0500

    Test

--- greedy.js
+++ greedy.js
@@ -1,40 +1,48 @@
-const container = document.querySelector('.dice-container');
 
+// array of dice images
+const diceImgs = ["images/dice1.png", "images/dice2.png", "images/dice3.png", "images/dice4.png", "images/dice5.png", "images/dice6.png"];
 
-function game(){
-    let dice = 6;
-    let score = 0;
+// buttons
+const rollButton = document.querySelector('.roll-button');
+rollButton.onclick = function() {rollButton();}
 
-    while (dice > 0){
-        let answer = prompt("Current Score: " + score +
-        "\nNumber of dice: " + dice + "\nRoll? yes or no: ");
-        answer = answer.toLowerCase();
+// every dice element
+const diceContainers = document.querySelectorAll('.dice');
 
-        if (answer == 'yes'){
-            let greedy = roll(dice);
-    
-            if (greedy[0] == 0){
-                console.log("you skunked!");
-                return;
-            }
-    
-            score += greedy[0];
-            dice -= greedy[1];
-    
-            if (dice == 0)
-                dice = 6;
-        }
-        else {
-            console.log("you take " + score + " points!");
-            dice = 0;
-        }
+// global variables
+let diceNumber = 0;
+let dice = 6;
+let score = 0;
+
+function rollButton(){
+    let greedy = roll(dice);
+
+    if (greedy[0] == 0){
+        console.log("you skunked!");
+        return;
     }
+    
+    score += greedy[0];
+    dice -= greedy[1];
+
+    if (dice == 0)
+        dice = 6;
 }
+
+function passButton(){
+    console.log("you take " + score + " points!");
+    dice = 0;
+}
+
 
 function roll(dice){
     let rolls = [];
     for (let i = 0; i < dice; i++){
         rolls[i] = Math.floor((Math.random() * 6) + 1);
+        const diceImage = document.createElement('img');
+        diceImage.src = diceImgs[rolls[i] - 1];
+        diceContainers[diceNumber].appendChild(diceImage);
+        diceNumber++;
         console.log(rolls[i]);
     }
     let game = score(rolls);
