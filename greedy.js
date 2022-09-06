cd168d418618849713f3d242ae01b59ecb06ba50
Sam Foit <sfoit2@huskers.unl.edu>
2022-09-06 13:22:09 -0500

    Fixed countdown format

--- greedy.js
+++ greedy.js
@@ -396,6 +396,13 @@ function countdownMidnight(){
         let minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
         let seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
 
+        if (hours < 10)
+            hours = `0${hours}`;
+        if (minutes < 10)
+            minutes = `0${minutes}`;
+        if (seconds < 10)
+            seconds = `0${seconds}`;
+
         timer.textContent = `${hours}:${minutes}:${seconds}`;
 
         if (timer.style.display == 'block'){
